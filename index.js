1c61d539208ed8004e8a6e39eb248306002f8471
yoandrypa <yoandrypa@gmail.com>
2022-09-19 15:45:16 -0400

    .

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-export * from './src/utils/request';
-export * from './src/utils/session';
-export * from './src/utils/localStorage';
+import _request from './src/utils/request';
+import _session from './src/utils/session';
+import _localStorage from './src/utils/localStorage';
+
+export const request = _request;
+export const session = _session;
+export const localStorage = _localStorage;
--- src/embeded/index.js
+++ src/embeded/index.js
@@ -1,3 +1,7 @@
-export * from './utils/request';
-export * from '../utils/session';
-export * from '../utils/localStorage';
+import _request from './utils/request';
+import _session from '../utils/session';
+import _localStorage from '../utils/localStorage';
+
+export const request = _request;
+export const session = _session;
+export const localStorage = _localStorage;
